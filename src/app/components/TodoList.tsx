9fa48c7ed4ef36a77ac23d570ebefcec83fb44aa
yamada <shinya.yamada@benext-corp.com>
2023-08-15 10:32:11 +0900

    ✨ Set TodoList component

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <ul className="space-y-3">
+      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 shadow rounded">
+        <span>散歩</span>
+        <div>
+          <button className="text-green-500 px-2">Edit</button>
+          <button className="text-orange-400 px-2">Delete</button>
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 export default TodoList;
