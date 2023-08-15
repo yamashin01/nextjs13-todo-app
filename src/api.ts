cac40f1667caaea80a0e042bf34e21469f51d865
yamada <shinya.yamada@benext-corp.com>
2023-08-15 11:39:40 +0900

    ✨ Add addTask api

--- src/api.ts
+++ src/api.ts
@@ -7,3 +7,16 @@ export const getAllTodos = async (): Promise<Task[]> => {
 
   return todos;
 };
+
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
+
+  return newTodo;
+};
